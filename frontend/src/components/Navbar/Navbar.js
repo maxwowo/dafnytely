f06f6703f09524d0d05974862d3d9f942acd1dbf
Max <maxwo@protonmail.com>
2019-10-28 17:47:11 +1100

    Did some shit

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Layout, Menu } from 'antd';
+import logo from '../../assets/logo.png';
 import styles from './Navbar.module.less';
 
 const Navbar = () => (
-  <Layout.Header className={styles.Navbar}>
-    <div className="logo"/>
+  <Layout.Header>
+    <img
+      src={logo}
+      className={styles.logo}
+      alt='Logo'
+    />
     <Menu
       theme="dark"
       mode="horizontal"
       style={{ lineHeight: '64px' }}
     >
-      <Menu.Item key="1">nav 1</Menu.Item>
-      <Menu.Item key="2">nav 2</Menu.Item>
-      <Menu.Item key="3">nav 3</Menu.Item>
+      <Menu.Item key="query">Query</Menu.Item>
+      <Menu.Item key="donate">Donate</Menu.Item>
+      <Menu.Item key="admin">Admin</Menu.Item>
     </Menu>
   </Layout.Header>
 );
