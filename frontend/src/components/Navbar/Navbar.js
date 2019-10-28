6d1078264bd27ea483e68394b0ff8613f963c584
Max <maxwo@protonmail.com>
2019-10-28 17:58:12 +1100

    Style

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -4,20 +4,37 @@ import logo from '../../assets/logo.png';
 import styles from './Navbar.module.less';
 
 const Navbar = () => (
-  <Layout.Header>
+  <Layout.Header
+    className={styles.header}
+  >
     <img
       src={logo}
       className={styles.logo}
       alt='Logo'
     />
     <Menu
-      theme="dark"
+      theme="light"
       mode="horizontal"
-      style={{ lineHeight: '64px' }}
+      className={styles.menu}
     >
-      <Menu.Item key="query">Query</Menu.Item>
-      <Menu.Item key="donate">Donate</Menu.Item>
-      <Menu.Item key="admin">Admin</Menu.Item>
+      <Menu.Item
+        key="query"
+        className={styles.menuItem}
+      >
+        Query
+      </Menu.Item>
+      <Menu.Item
+        key="donate"
+        className={styles.menuItem}
+      >
+        Donate
+      </Menu.Item>
+      <Menu.Item
+        key="admin"
+        className={styles.menuItem}
+      >
+        Admin
+      </Menu.Item>
     </Menu>
   </Layout.Header>
 );
