c00c2ce62ebd4160862558edc5b8ae1c75aa8d3b
Max <maxwo@protonmail.com>
2019-10-28 15:33:33 +1100

    Did some shit

--- frontend/src/pages/QueryPage/QueryPage.js
+++ frontend/src/pages/QueryPage/QueryPage.js
@@ -1,29 +1,59 @@
 import React from 'react';
 import FullSizeLayout from '../../components/FullSizeLayout/FullSizeLayout';
-import { Icon, Layout, Menu } from 'antd';
+import { Layout, Menu, Select, DatePicker } from 'antd';
 import Center from '../../components/Center/Center';
 
 const QueryPage = () => {
+
+  const bloodTypes = ['O-', 'O+', 'A-', 'A+', 'B-', 'B+', 'AB-', 'AB+'];
+
+  const onBloodTypeChange = (
+    value
+  ) => {
+    console.log(value);
+  };
+
+  const onMinimumExpiryChange = (
+    value
+  ) => {
+    console.log(value);
+  };
+
   return (
     <FullSizeLayout>
       <Layout.Sider>
         <Menu
           theme='dark'
         >
-          <Menu.Item>
-            <Icon
-              type='filter'
-            />
-            <span>
-              boom boom
-            </span>
-          </Menu.Item>
-          <Menu.Item>
-            <Icon
-              type='filter'
-            />
-            <span>bang bang</span>
-          </Menu.Item>
+          <Select
+            placeholder="Filter by blood type"
+            optionFilterProp="children"
+            style={{ width: '100%' }}
+            onChange={onBloodTypeChange}
+            filterOption={(input, option) =>
+              option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+            }
+          >
+            {
+              bloodTypes.map(
+                el => (
+                  <Select.Option
+                    value={el}
+                    key={el}
+                  >
+                    {`Type ${el}`}
+                  </Select.Option>
+                )
+              )
+            }
+          </Select>
+
+          <DatePicker
+            onChange={onMinimumExpiryChange}
+            mode='date'
+            placeholder='Filter by minimum expiry date'
+          />
+
         </Menu>
       </Layout.Sider>
 
