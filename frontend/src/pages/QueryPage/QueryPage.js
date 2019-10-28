31937e2debe6152604d739dfd46855c1bf142fa9
Max <maxwo@protonmail.com>
2019-10-28 15:47:37 +1100

    Did some shit

--- frontend/src/pages/QueryPage/QueryPage.js
+++ frontend/src/pages/QueryPage/QueryPage.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import FullSizeLayout from '../../components/FullSizeLayout/FullSizeLayout';
-import { Layout, Menu, Select, DatePicker } from 'antd';
-import Center from '../../components/Center/Center';
+import { Layout, Menu, Select, DatePicker, List, Descriptions, Typography } from 'antd';
 
 const QueryPage = () => {
 
   const bloodTypes = ['O-', 'O+', 'A-', 'A+', 'B-', 'B+', 'AB-', 'AB+'];
+  const bloodList = [
+    {
+      'type': 'A',
+      'arrival_date': '2019-08-12',
+      'use-by-date': '2019-09-12',
+      'donor-id': 'D-12345',
+      'lab-id': 'L-12345',
+      'volume-ml': 500
+    }
+  ];
 
   const onBloodTypeChange = (
     value
@@ -21,7 +30,9 @@ const QueryPage = () => {
 
   return (
     <FullSizeLayout>
-      <Layout.Sider>
+      <Layout.Sider
+        style={{ padding: 10 }}
+      >
         <Menu
           theme='dark'
         >
@@ -57,10 +68,29 @@ const QueryPage = () => {
         </Menu>
       </Layout.Sider>
 
-      <Layout.Content>
-        <Center>
-          Some content
-        </Center>
+      <Layout.Content
+        style={{ padding: 10 }}
+      >
+        <List
+          header={<Typography.Title style={{ textAlign: 'center' }}>Blood list</Typography.Title>}
+          bordered
+          dataSource={bloodList}
+          renderItem={item => (
+            <List.Item>
+              <Descriptions
+                title={item.type}
+                style={{ padding: 10 }}
+              >
+                <Descriptions.Item label="Type">{item.type}</Descriptions.Item>
+                <Descriptions.Item label="Arrival Date">{item['arrival_date']}</Descriptions.Item>
+                <Descriptions.Item label="Use by date">{item['use-by-date']}</Descriptions.Item>
+                <Descriptions.Item label="Donor ID">{item['donor-id']}</Descriptions.Item>
+                <Descriptions.Item label="Lab ID">{item['lab-id']}</Descriptions.Item>
+                <Descriptions.Item label="Volume">{item['volume-ml']}</Descriptions.Item>
+              </Descriptions>
+            </List.Item>
+          )}
+        />
       </Layout.Content>
     </FullSizeLayout>
   )
