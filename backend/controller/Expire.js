a86c842815d5fe9ff28a793599839f0be55278cf
samrathacharya <z5161678@student.unsw.edu.au>
2019-11-06 13:04:41 +1100

    Changed forEach to while

--- backend/controller/Expire.js
+++ backend/controller/Expire.js
@@ -1,50 +1,53 @@
-const database = require('../model/db');
+const database = require("../model/db");
 
 class Expire {
-
   static async discard_by_id(body) {
     let t = { status: true };
     let i = 0;
-    database.db.bloods.forEach(blood => {
-      if (blood.id == body.id) {
-        if (!database.blood_expire(new Date(blood.use_by_date))) {
-          t = { status: 'target blood is not expired' };
+    while (i < database.db.bloods.length) {
+      if (database.db.bloods[i].id == body.id) {
+        if (
+          !database.blood_expire(new Date(database.db.bloods[i].use_by_date))
+        ) {
+          t = { status: "target blood is not expired" };
         } else {
           database.db.delete_blood(i);
         }
-
       }
       i++;
-    });
+    }
     return t;
-
   }
 
   static async discard_all(body) {
     let i = 0;
     let remove = [];
-    database.db.bloods.forEach((blood, i) => {
-
-      if (database.blood_expire(new Date(blood.use_by_date))) {
+    while (i < database.db.bloods.length) {
+      if (database.blood_expire(new Date(database.db.bloods[i].use_by_date))) {
         remove.unshift(i);
       }
       i++;
-    });
-    remove.forEach(a => database.db.delete_blood(a));
+    }
+
+    i = 0;
+    while (i < remove.length) {
+      database.db.delete_blood(remove[i]);
+      i++;
+    }
     return { status: database.db.bloods };
   }
 
   static async get_all(body) {
     let r = [];
-    database.db.bloods.forEach((blood) => {
-      if (database.blood_expire(new Date(blood.use_by_date))) {
-        r.push(blood);
+    let i = 0;
+    while (i < database.db.bloods.length) {
+      if (database.blood_expire(new Date(database.db.bloods[i].use_by_date))) {
+        r.push(database.db.bloods[i]);
       }
-    });
+      i++;
+    }
     return { list: r };
   }
-
-
 }
 
 module.exports = Expire;
