86d75f91b76b8cbfc7b2cc460b63e74c5d530aaa
Harold Newman <hnewman2744@gmail.com>
2019-11-19 11:10:36 +1100

    Modified add.js

--- backend/controller/Add.js
+++ backend/controller/Add.js
@@ -1,16 +1,19 @@
 const Bank = require('../model/Bank');
+const Unit = require('../schema/Unit');
 
 class Add {
   static async add_blood(body) {
     // Get Blood Units from request as list, if unit is expired, do not add it to list
     let new_units = []
     for (let i = 0; i < body.bloods.length; i++) {
-      unit = new Unit(Bank.blood_id, element.type, element.arrival_date, element.use_by_date, element.donor_id, element.lab_id);
+      let unit = new Unit(Bank.blood_id, body.bloods[i].type, body.bloods[i].arrival_date, body.bloods[i].use_by_date, body.bloods[i].donor_id, body.bloods[i].lab_id);
       if (!unit.expired()) {
         new_units.push(unit);
+      } else {
+        return { status: 'expire item' };
       }
     }
-    
+    console.log(new_units);
     // For each new unit add it to the blood bank
     for (let i = 0; i < new_units.length; i++) {
       // DAFNY verified add_unit function
